68925021dd626a5214739d5acb536949cdf93ff0
farnaf <72419925+farnaf@users.noreply.github.com>
2021-11-06 22:14:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,8 +16,8 @@ const urlsToCache = ['assets/scripts/main.js',
                      'assets/images/icons/5-star.svg',
                      'assets/images/icons/arrow-down.png',
                      'favicon.ico',
-                     'sw.js',
-                     '/'
+                     './sw.js',
+                     './'
                     ]; 
 
 // Once the service worker has been installed, feed it some initial URLs to cache

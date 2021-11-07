bc5c2a28b53d40cf00f23fb6c632960b927ffa91
Fhuynh1 <81829426+Fhuynh1@users.noreply.github.com>
2021-11-06 18:17:01 -0700

    Lab 7 Done?
    Co-authored-by: farnaf <farnaf@users.noreply.github.com>

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -49,6 +50,24 @@ export class Router {
    */
   navigate(page, statePopped) {
     console.log(`navigate() function called, requested page: ${page}`);
+    if (!this[page]){
+      console.log('The function was not found.');
+      return;
+    }
+
+    var hash;
+    if (page == 'home'){
+      hash = '';
+    } else {
+      hash = '#' + page;
+    }
+
+    if (!statePopped && window.location.hash != hash){
+      history.pushState({'page' : page},'',window.location.origin+hash);
+    }
+
+    this[page](); // call the function 
+    
     /**
      * TODO - Part 1 - Step 4
      * Now, we are going to call the functions that we stored earlier based on 
